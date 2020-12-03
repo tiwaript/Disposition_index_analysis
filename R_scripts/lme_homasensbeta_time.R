3cb4ae586dd4bfe621c683aff7944aaef0a835be
tiwaript <pradeep.tiwari@igib.in>
2020-12-03 16:53:29 +0530

    Rate of change with time, LME model

--- R_scripts/lme_homasensbeta_time.R
+++ R_scripts/lme_homasensbeta_time.R
@@ -3,7 +3,8 @@ require(lme4)
 library(nlme)
 ##################################
 ####### linear mixed effect model #########
-summary(lme(homasens~age*cgly_st_18yr,random=~1|key,
+require(broom)
+tidy(lme(homasens~age*cgly_st_18yr,random=~1|key,
     data=na.omit(dat_lme_long[dat_lme_long$sex=="Female",c("key","homasens","age","cgly_st_18yr")])))
 summary(lme(homasens~age*cgly_st_18yr,random=~1|key,
             data=na.omit(dat_lme_long[dat_lme_long$sex=="Male",c("key","homasens","age","cgly_st_18yr")])))
@@ -20,8 +21,7 @@ summary(lme(dispindex~age*cgly_st_18yr,random=~1|key,
 summary(lme(dispindex~age*cgly_st_18yr,random=~1|key,
             data=na.omit(dat_lme_long[dat_lme_long$sex=="Male",c("key","dispindex","age","cgly_st_18yr")])))
 ####### linear model for comparison with LME model ########################
-
-summary(lm(homabeta~age+sex+cgly_st_18yr,data=na.omit(dat_lme_long)))
+tidy(lm(homabeta~age+sex+cgly_st_18yr,data=na.omit(dat_lme_long)))
 summary(lm(homasens~age+sex+cgly_st_18yr,data=na.omit(dat_lme_long)))
 summary(lm(dispindex~age+sex+cgly_st_18yr,data=na.omit(dat_lme_long)))
 ####### ###################################################################
